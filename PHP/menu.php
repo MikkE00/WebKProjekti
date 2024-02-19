0496b379aab89d40cff1224bc81ef292a72a1571
MikkE00 <mikael.elizarov@student.hamk.fi>
2024-02-19 10:57:51 +0200

    Updated menu
    updated
    
    Co-Authored-By: Kalle-Github <142879782+Kalle-Github@users.noreply.github.com>

--- PHP/menu.php
+++ PHP/menu.php
@@ -5,56 +5,7 @@
     <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Burgeriravintolan Menu</title>
-
-
-    <style>
-      body {
-        background-image: url('img/taustaBurgeri.webp');
-        background-size: cover;
-        background-attachment: fixed;
-        background-position: center;
-        backdrop-filter: blur(10px);
-      }
-    </style>
-
-</head>
-<body>
-    <main>
-        <div>
-          <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img width="110px"max-width="100%" height="auto" src="img/logoTransparent.png" width="150px"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse show" id="navbarDark">
-                  <ul class="navbar-nav me-auto mb-2 mb-xl-0">
-                    <li class="nav-item navbarH">
-                      <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item navbarH">
-                      <a class="nav-link active " aria-current="page" href="Aboutus.html" >About us</a>
-                    </li>
-                    <li class="nav-item navbarH">
-                      <a class="nav-link active" href="menu.html">Menu</a>
-                    <li class="nav-item navbarH">
-                      <a class="nav-link active" href="Contact.html" tabindex="-1" aria-disabled="true">Feedback</a>
-                    </li>
-                  </ul>
-                  </form>
-                </div>
-              </div>
-            </nav>
-      </div>
-      <!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Burgeriravintolan Menu</title>
+    <title>Burgeriravintolan Menu!!!</title>
 
 
     <style>
@@ -127,7 +78,7 @@
         }
       }
       ?>
-
+<footer>Kokeilu</footer>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </main>
